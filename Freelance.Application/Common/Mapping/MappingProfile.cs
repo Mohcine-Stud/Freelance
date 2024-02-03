a05e2c03d615ca14064323905d25da03f45175d1
ELGHAZAL-SAID <said.elghazal@edu.uiz.ac.ma>
2024-02-03 13:08:27 +0100

    change Mapster to AutoMapper

---
+++ Freelance.Application/Common/Mapping/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using Freelance.Application.Authentication.Commands.Register;
+using Freelance.Application.ViewModels.Authentication;
+using AutoMapper;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Freelance.Application.Common.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+
+    }
+}
