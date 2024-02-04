e0876ff9d1beec85b654256c9ec911dc1f6b42d6
ELGHAZAL-SAID <said.elghazal@edu.uiz.ac.ma>
2024-02-04 13:55:08 +0100

    fixed

--- Freelance.Application/Services/Condidate/CandidatService/CandidatService.cs
+++ Freelance.Application/Services/Condidate/CandidatService/CandidatService.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-public class CandidatService : ICondidateService
+public class CandidatService : ICandidateService
 {
 
     private readonly IGenericRepository<Candidat> _condidateRepository;
