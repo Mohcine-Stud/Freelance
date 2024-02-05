a425357d221aa8e1aba2a635967f9f15f4de4cd5
ELGHAZAL-SAID <said.elghazal@edu.uiz.ac.ma>
2024-02-05 12:32:41 +0100

    done with services

--- Freelance.Application/Services/Condidate/CondidatCompService/CondidatCompService.cs
+++ Freelance.Application/Services/Condidate/CondidatCompService/CondidatCompService.cs
@@ -1,37 +1,57 @@
-﻿using Freelance.Application.ViewModels.DTOs.CondidatCompDTO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using Freelance.Application.Persistence.IRepositories;
+using Freelance.Application.ViewModels.DTOs.CondidatCompDTO;
+using Freelance.Domain.Models;
 
-namespace Freelance.Application.Services.Condidate.CondidatCompService
+
+namespace Freelance.Application.Services.Condidate.CondidatCompService;
+
+public class CondidatCompService : ICondidatCompService
 {
-    internal class CondidatCompService : ICondidatCompService
+    readonly IGenericRepository<CondidatComp> _condidatComp;
+    private readonly IMapper _mapper;
+
+    public CondidatCompService(IGenericRepository<CondidatComp> condidatComp, IMapper mapper)
     {
-        public Task<CondidatCompDTO> CreateAsync(CondidatCompCreateDTO entity)
-        {
-            throw new NotImplementedException();
-        }
+        _mapper = mapper;
+        _condidatComp = condidatComp;
+    }
 
-        public Task DeleteAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<CondidatCompDTO> CreateAsync(CondidatCompCreateDTO entity)
+    {
+        var competenceDmExpertise = _mapper.Map<CondidatComp>(entity);
+        var createdcompetenceDm = await _condidatComp.PostAsync(competenceDmExpertise);
+        return _mapper.Map<CondidatCompDTO>(createdcompetenceDm);
+    }
 
-        public Task<List<CondidatCompDTO>> FindAllAsync()
-        {
-            throw new NotImplementedException();
-        }
+    public async Task DeleteAsync(int id)
+    {
+        var existingcompetenceDm = await _condidatComp.GetAsync(id);
+        if (existingcompetenceDm == null)
+            return;
+        await _condidatComp.DeleteAsync(id);
+    }
 
-        public Task<CondidatCompDTO> FindByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<List<CondidatCompDTO>> FindAllAsync()
+    {
+        var competenceDmExpertise = await _condidatComp.GetAllAsync();
+        return _mapper.Map<List<CondidatCompDTO>>(competenceDmExpertise);
+    }
 
-        public Task<CondidatCompDTO> UpdateAsync(int id, CondidatCompUpdateDTO entity)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<CondidatCompDTO> FindByIdAsync(int id)
+    {
+        var competenceDmExpertise = await _condidatComp.GetAsync(id);
+        return _mapper.Map<CondidatCompDTO>(competenceDmExpertise);
+    }
+
+    public async Task<CondidatCompDTO> UpdateAsync(int id, CondidatCompUpdateDTO entity)
+    {
+        var existingcompetenceDm = await _condidatComp.GetAsync(id);
+        if (existingcompetenceDm == null)
+            return null;
+
+        _mapper.Map(entity, existingcompetenceDm);
+        await _condidatComp.PutAsync(id, existingcompetenceDm);
+        return _mapper.Map<CondidatCompDTO>(existingcompetenceDm);
     }
 }
