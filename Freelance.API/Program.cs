edbc44c7cd0606fbc58e821df0969af4e2fa86d2
ELGHAZAL-SAID <said.elghazal@edu.uiz.ac.ma>
2024-02-06 15:45:17 +0100

    adding Cors

--- Freelance.API/Program.cs
+++ Freelance.API/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -29,6 +30,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+app.UseCors(builder =>
+{
+    builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+});
 
 //app.UseExceptionHandler("/error");
 app.UseHttpsRedirection();
